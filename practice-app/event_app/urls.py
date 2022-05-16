01d4a4dfd02756b86ab3a375aa69fe01e364a9b8
ozdemirfurkan <furkan.ozdemir1@boun.edu.tr>
2022-05-16 20:08:29 +0300

    Added github info function

--- practice-app/event_app/urls.py
+++ practice-app/event_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('sign_in', views.sign_in, name="sign_in"),
     path('sign_up', views.sign_up, name="sign_up"),
     path('home', views.home, name="home"),
-    path('user_login', views.user_login, name="user_login")
+    path('user_login', views.user_login, name="user_login"),
+    path('viewGithubInfo', views.viewGithubInfo, name="viewGithubInfo"),
+    path('viewGithubInfoPage', views.viewGithubInfoPage, name="viewGithubInfoPage"),
 ]
