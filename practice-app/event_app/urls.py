5332b90311a80d8475a410c29a331b4797a68843
ozdemirfurkan <furkan.ozdemir1@boun.edu.tr>
2022-05-17 11:37:40 +0300

    Added sign up functionality

--- practice-app/event_app/urls.py
+++ practice-app/event_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sign_in', views.sign_in, name="sign_in"),
     path('sign_up', views.sign_up, name="sign_up"),
+    path('addUser', views.addUser, name="addUser"),
     path('home', views.home, name="home"),
     path('user_login', views.user_login, name="user_login"),
     path('viewGithubInfo', views.viewGithubInfo, name="viewGithubInfo"),
