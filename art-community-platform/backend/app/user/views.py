5066380ad4096a7d138a21ddc7b49944ea2aa6ee
Burak Ferit Aktan <aktan.burakfa@gmail.com>
2022-10-24 22:57:42 +0300

    register(signup) functionality added

---
+++ art-community-platform/backend/app/user/views.py
@@ -0,0 +1,72 @@
+import re
+
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseBadRequest
+from django.core.exceptions import BadRequest
+from rest_framework.decorators import api_view
+import hashlib
+import json
+import random
+from .models import User
+from .serializers import UserSerializer
+from urllib import response
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.mail import send_mail
+
+
+@api_view(['GET'])
+def health_check(request):
+    return HttpResponse("Health check is successful, app is healthy.")
+
+
+@api_view(['POST'])
+def signup(req):
+    username = req.data['username']
+    password = req.data['password']
+    email = req.data['email']
+    token = hashlib.sha256((username+password+email).encode('utf-8')).hexdigest()
+    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    print("email is: ", email)
+    m = re.match(r"(\w|\.)*@\w*.com", email)
+    print("email is: ", email)
+    if m is None:
+        return HttpResponseBadRequest("email is invalid")
+    print("email is: ", email)
+    if len(username) < 5:
+        return HttpResponseBadRequest("username can't be shorter than 5 characters")
+    print("email is: ", email)
+    if len(password) < 8:
+        return HttpResponseBadRequest("password can't be shorter than 8 characters")
+    lower = False
+    upper = False
+    digit = False
+    for p in password:
+        if ord("0") <= ord(p) <= ord("9"):
+            digit = True
+        if ord("a") <= ord(p) <= ord("z"):
+            lower = True
+        if ord("A") <= ord(p) <= ord("Z"):
+            upper = True
+    if not upper:
+        return HttpResponseBadRequest("password should contain at least one upper case letter")
+    if not lower:
+        return HttpResponseBadRequest("password should contain at least one lower case letter")
+    if not digit:
+        return HttpResponseBadRequest("password should contain at least one numerical digit")
+    try:
+        print("trya girdik")
+        obj = User.objects.get(email=email)
+        return HttpResponseBadRequest("user with this email already exists")
+        obj = User.objects.get(username=username)
+        return HttpResponseBadRequest("user with this username already exists")
+    except :
+        User.objects.create(username=username, password=hashed_password, email=email, token=token)
+    return HttpResponse("ok")
+
+
+@api_view(['GET'])
+def users(req):
+    all_users = User.objects.all()  # fetch data from db
+    serializer = UserSerializer(all_users, many=True)  # convert to JSON format
+    return Response(serializer.data)
