c4a8d31da3033118b3857bab7b1e418f4e592232
Yamur <yagmur.goktas@eatron.com>
2022-10-27 13:41:22 +0300

    input container style defined

--- art-community-platform/mobile/screens/Login/Signup.js
+++ art-community-platform/mobile/screens/Login/Signup.js
@@ -17,3 +17,25 @@ const Signup = () => {
 };
 
 export default Signup;
+
+const styles = StyleSheet.create({
+
+  inputContainer: {
+    
+    justifyContent:'flex-end',
+    width: "80%",
+    padding: 16,
+    marginVertical: 24,
+    borderRadius: 12,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
