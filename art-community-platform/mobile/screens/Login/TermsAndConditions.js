bef3a7738d0b8f4546abb06f9987f56f19206156
Yamur <yagmur.goktas@eatron.com>
2022-10-28 21:57:19 +0300

    class definition

--- art-community-platform/mobile/screens/Login/TermsAndConditions.js
+++ art-community-platform/mobile/screens/Login/TermsAndConditions.js
@@ -1,3 +1,8 @@
 import React , {Component} from 'react';
 import { View, Text, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 
+class TermsAndConditions extends Component{
+
+}
+
+export default TermsAndConditions;
