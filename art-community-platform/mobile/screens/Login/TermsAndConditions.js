04e34558fbb8aa9298121302c80b87e0bd5851e6
Yamur <yagmur.goktas@eatron.com>
2022-10-28 21:59:49 +0300

    container style added

--- art-community-platform/mobile/screens/Login/TermsAndConditions.js
+++ art-community-platform/mobile/screens/Login/TermsAndConditions.js
@@ -2,7 +2,21 @@ import React , {Component} from 'react';
 import { View, Text, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 
 class TermsAndConditions extends Component{
-
+    render(){
+        return (
+            <View style={styles.container}></View>
+        );
+    }
 }
 
+
 export default TermsAndConditions;
+
+const styles = {
+
+    container:{
+      marginTop: 20,
+      marginLeft: 10,
+      marginRight: 10
+    },
+}
