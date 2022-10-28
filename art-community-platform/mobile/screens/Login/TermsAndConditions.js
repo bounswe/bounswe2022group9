0d1bc32374007194e33e08aafae14e4190701792
Yamur <yagmur.goktas@eatron.com>
2022-10-28 22:00:42 +0300

    Title added

--- art-community-platform/mobile/screens/Login/TermsAndConditions.js
+++ art-community-platform/mobile/screens/Login/TermsAndConditions.js
@@ -4,7 +4,9 @@ import { View, Text, ScrollView, Dimensions, TouchableOpacity } from 'react-nati
 class TermsAndConditions extends Component{
     render(){
         return (
-            <View style={styles.container}></View>
+            <View style={styles.container}>
+                <Text style={styles.title}>Terms and conditions</Text>
+            </View>
         );
     }
 }
@@ -18,5 +20,9 @@ const styles = {
       marginTop: 20,
       marginLeft: 10,
       marginRight: 10
+    },
+    title: {
+        fontSize: 22,
+        alignSelf: 'center'
     },
 }
