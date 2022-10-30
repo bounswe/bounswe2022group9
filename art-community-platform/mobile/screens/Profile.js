6216bb2fb62789993138e378786902016524fa0a
Yamur <yagmur.goktas@eatron.com>
2022-10-30 21:33:24 +0300

    Profile photo added

--- art-community-platform/mobile/screens/Profile.js
+++ art-community-platform/mobile/screens/Profile.js
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 const Profile = () => {
   return (
     <View style={styles.container}>
           <View style={styles.header}></View>
-    
+          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
     
     </View>
   );
@@ -16,5 +16,16 @@ const styles = StyleSheet.create({
   header:{
     backgroundColor: "#173679",
     height:200,
-  }
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
 });
