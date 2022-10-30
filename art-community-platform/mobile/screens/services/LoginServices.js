1c1df59246bc90e8ce9cdc6daf144e2840e0c814
Yamur <yagmur.goktas@eatron.com>
2022-10-31 00:04:53 +0300

    login axios func added

--- art-community-platform/mobile/screens/services/LoginServices.js
+++ art-community-platform/mobile/screens/services/LoginServices.js
@@ -1,5 +1,30 @@
 import axios from "axios";
 
+let token = undefined;
+export const login = async (username, password) => {
+  return axios
+    .post(
+      "http://ec2-44-202-130-117.compute-1.amazonaws.com/user/login",
+      {
+        username: username,
+        password: password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      token = response.body.token;
+      console.log(token)
+      return response;
+    })
+    .catch((error) => {
+      return error.response;
+    });
+};
+
 export const signup = async (username, email, password) => {
   return axios
     .post(
