88b479823e549ccff037cded84b2e386493b08fc
Yamur <yagmur.goktas@eatron.com>
2022-10-30 21:44:48 +0300

    Follower and followees number displayed

--- art-community-platform/mobile/screens/Profile.js
+++ art-community-platform/mobile/screens/Profile.js
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View, Image } from "react-native";
 const Profile = () => {
   return (
     <View style={styles.container}>
-          <View style={styles.header}></View>
+          <View style={styles.header}>
+            <Text style={[styles.follower , {marginTop:60}]}> Followers: 0</Text>
+            <Text style={styles.follower}> Followees: 0</Text>
+          </View>
           <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
           <View style={{marginTop:40}}>
           <View style={styles.bodyContent}></View>
@@ -31,4 +34,16 @@ const styles = StyleSheet.create({
     position: 'absolute',
     marginTop:130
   },
+  bodyContent: {
+    flex: 1,
+    padding:30,
+  },
+  follower: {
+    alignSelf:'center',
+    color:'white',
+    fontWeight: "bold"
+  },
+  followee:{
+
+  }
 });
