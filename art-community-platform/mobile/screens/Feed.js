278b4f3cf522d37a870e164445b31b13a79fc077
Bugra Elmas <bugra@medicasimple.com>
2022-11-01 12:13:38 +0300

    small ui fix

--- art-community-platform/mobile/screens/Feed.js
+++ art-community-platform/mobile/screens/Feed.js
@@ -1,66 +1,65 @@
-
 import { CurrentRenderContext } from "@react-navigation/native";
-import { StyleSheet, Text, View, Image,ScrollView, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import Post from "./components/Post";
 
 const Feed = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <ScrollView>
-        <View style={styles.square}>
-          <View style={styles.header}>
-            <Text>artLand</Text>
-          </View>
-          <Image style={styles.photo} source = {require("../assets/halloween.png")} />
-        </View>
+        <Post
+          username="artLand"
+          uri={require("../assets/halloween.png")}
+          date="30m"
+        />
 
-        <View style={styles.square}>
-          <View style={styles.header}>
-            <Text>love.art</Text>
-          </View>
-          <Image style={styles.photo} source = {require("../assets/concert.jpg")} />
-        </View>
+        <Post
+          username="love.art"
+          uri={require("../assets/concert.jpg")}
+          date="1h"
+        />
 
-        <View style={styles.square}>
-          <View style={styles.header}>
-            <Text>photo_community</Text>
-          </View>
-          <Image style={styles.photo} source = {require("../assets/old-man.jpg")} />
-        </View>
+        <Post
+          username="photo_community"
+          uri={require("../assets/old-man.jpg")}
+          date="1h"
+        />
 
-        <View style={styles.square}>
-          <View style={styles.header}>
-            <Text>112233paint</Text>
-          </View>
-          <Image style={styles.photo} source = {require("../assets/halloween-gaf.jpg")} />
-        </View>
+        <Post
+          username="112233paint"
+          uri={require("../assets/halloween-gaf.jpg")}
+          date="2h"
+        />
 
-        <View style={styles.square}>
-          <View style={styles.header}>
-            <Text>nature_photo</Text>
-          </View>
-          <Image style={styles.photo} source = {require("../assets/witch.jpg")} />
-        </View>
+        <Post
+          username="nature_photo"
+          uri={require("../assets/witch.jpg")}
+          date="3h"
+        />
 
-        <View style={styles.square}>
-          <View style={styles.header}>
-            <Text>safariPhotographer</Text>
-          </View>
-          <Image style={styles.photo} source = {require("../assets/timothy-barlin.jpg")} />
-        </View>
-        
-    </ScrollView>
+        <Post
+          username="safariPhotographer"
+          uri={require("../assets/timothy-barlin.jpg")}
+          date="5h"
+        />
+      </ScrollView>
     </View>
-    
   );
 };
 
 export default Feed;
 const styles = StyleSheet.create({
-  header:{
+  header: {
     backgroundColor: "white",
-    height:50,
+    height: 50,
   },
-  square:{
+  square: {
     padding: 12,
     paddingHorizontal: 12,
     marginTop: 12,
@@ -68,18 +67,16 @@ const styles = StyleSheet.create({
     marginRight: 12,
     marginBottom: 12,
     borderRadius: 6,
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: "white",
   },
-  photo:{
-    alignSelf: 'center',
-    width:300,
-    height:300,
-    marginTop:12,
-    marginBottom:12,
-    marginLeft:12,
-    marginRight:12,
-  }
+  photo: {
+    alignSelf: "center",
+    width: 300,
+    height: 300,
+    marginTop: 12,
+    marginBottom: 12,
+    marginLeft: 12,
+    marginRight: 12,
+  },
 });
-
-
