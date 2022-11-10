a6ceabfe35f4e5e9fa1b14cef09cba59aac2486c
Yamur <yagmur.goktas@eatron.com>
2022-11-10 22:41:44 +0300

    image display with square size

--- art-community-platform/mobile/screens/ArtItemDisplay.js
+++ art-community-platform/mobile/screens/ArtItemDisplay.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Text, StyleSheet} from "react-native";
+import { Text, StyleSheet, Image, View, Dimensions} from "react-native";
 
+const dimensions = Dimensions.get("window");
 const ArtItemDisplay = () => {
     return(
+        <View>
         <Text style= { styles.creator }>Yagmur Goktas</Text>
+        <Image style= { styles.photo } source = {require("../assets/bus.jpg")} /></View>
     )
 }
 
@@ -15,5 +18,11 @@ const styles = StyleSheet.create({
         alignSelf:'center',
         color:'#173679',
         fontWeight: "bold",
+    },
+    photo: {
+        marginTop: 30,
+        width: dimensions.width*0.70,
+        height: dimensions.width*0.70,
+        alignSelf:'center',
     },
 })
