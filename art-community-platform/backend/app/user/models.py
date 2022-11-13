e0777df5189062af1aa1448d439ef32a64008b82
ozdemirfurkan <furkan.ozdemir1@boun.edu.tr>
2022-11-13 16:38:30 +0300

    new models are added.

--- art-community-platform/backend/app/user/models.py
+++ art-community-platform/backend/app/user/models.py
@@ -1,16 +1,43 @@
-import datetime
-
-import django.utils.timezone
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(models.Model):
-    name = models.CharField(max_length=100, default='Artie')
-    birthdate = models.DateField(default=django.utils.timezone.now)
+    name = models.CharField(max_length=100)
+    birthdate = models.DateField()
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=256)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
     token = models.CharField(max_length=256)
+    profileImgURL = models.URLField(default="https://www.linkpicture.com/q/LPic6370e982af0ad569150138.png")
+    location = models.CharField(max_length=100, null=True)
+    artItems = ArrayField(models.IntegerField(), null=True)
+    followers = ArrayField(models.IntegerField(), null=True)
+    followings = ArrayField(models.IntegerField(), null=True)
+    favourites = ArrayField(models.IntegerField(), null=True)
+    comments = ArrayField(models.IntegerField(), null=True)
+    exhibitions = ArrayField(models.IntegerField(), null=True)
+    notifications = ArrayField(models.IntegerField(), null=True)
 
     def __str__(self):
         self.email + " " + self.token
+
+
+class ArtItem(models.Model):
+    ownerid = models.IntegerField()
+    imageURL = models.URLField()
+    description = models.CharField(max_length=512)
+    date = models.DateField()
+    tags = ArrayField(models.CharField(max_length=100), null=True)
+    comments = ArrayField(models.IntegerField(), null=True)
+    favourites = ArrayField(models.IntegerField(), null=True)
+
+
+class Comment(models.Model):
+    ownerid = models.IntegerField()
+    text = models.CharField(max_length=1024)
+    date = models.DateField()
+
+
+class Tag(models.Model):
+    text = models.CharField(max_length=100)
