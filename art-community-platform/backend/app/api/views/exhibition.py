42ef6b63683c21f1461ef34a402d029f54da9a7b
omersisman <ofsisman99@gmail.com>
2022-11-21 00:17:50 +0300

    create exhibition mock implemented

--- art-community-platform/backend/app/api/views/exhibition.py
+++ art-community-platform/backend/app/api/views/exhibition.py
@@ -15,6 +15,37 @@ from ..models.comment import Comment
 from ..models.tag import Tag
 
 
+@api_view(['POST'])
+def create_exhibition(req):
+    data = json.loads(req.body)
+    try:
+        owner_id = data['owner_id']
+        ex_type = data['type']
+        location = data['location']
+        date = data['date']
+        art_item_ids = data['art_items']
+    except:
+        return HttpResponse("missing fields", status=400)
+
+    try:
+        # TODO: Tag will change to Exhibition
+        e = Tag.objects.create(owner_id=owner_id, type=ex_type, location=location, date=date, art_items=art_item_ids)
+    except:
+        return HttpResponse('exhibition can not created', status=400)
+
+    try:
+        owner = User.objects.get(id=e.owner_id)
+    except:
+        return HttpResponse('no owner found with this id', status=404)
+
+    art_items = []
+    for art_item_id in art_item_ids:
+        art_items.append(get_art_item_by_id_helper(art_item_id))
+
+    return JsonResponse({"id": e.id, "owner_name": owner.name, "type": e.type,
+                         "location": e.location, "date": e.date, "art_items": art_items})
+
+
 @api_view(['GET'])
 def get_exhibition_by_id(req, exhibition_id):
 
