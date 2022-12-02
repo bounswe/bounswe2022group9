3e5d4de8debe2842648483eb31e349a7e73f66f2
Yamur <yagmur.goktas@eatron.com>
2022-12-03 00:22:33 +0300

    get art item information from backend

---
+++ art-community-platform/mobile/screens/services/ArtItemService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const getArtItem = async (token, art_item_id) => {
+    return axios
+      .get(
+        "http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/art-items/:art_item_id",
+        {   
+            urlParams : {
+                art_item_id: art_item_id,
+        },
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token,
+          },
+        }
+      )
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        return error.response;
+      });
+  };
+
+
