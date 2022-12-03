e373b26a149a2a4f6dc4b7f9421cfcd7bf044d1a
Yamur <yagmur.goktas@eatron.com>
2022-12-04 00:01:49 +0300

    urls fixed

--- art-community-platform/mobile/screens/services/ArtItemService.js
+++ art-community-platform/mobile/screens/services/ArtItemService.js
@@ -3,11 +3,8 @@ import axios from "axios";
 export const getArtItem = async (token, art_item_id) => {
     return axios
         .get(
-            "http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/art-items/:art_item_id",
+            `http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/art-items/${art_item_id}`,
             {   
-                urlParams : {
-                    art_item_id: art_item_id,
-            },
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": token,
@@ -22,32 +19,10 @@ export const getArtItem = async (token, art_item_id) => {
         });
   };
 
-export const getLikeUsers = async (token, art_item_id) => {
-    return axios
-        .get(
-            "http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/art-items/:art_item_id/favourites",
-            {
-                urlParams : {
-                    art_item_id: art_item_id,
-                },
-                headers: {
-                "Content-Type": "application/json",
-                "Authorization": token,
-                },
-            }
-            )
-            .then((response) => {
-                return response;
-            })
-            .catch((error) => {
-                return error.response;
-            });
-};
-
 export const like = async (token, art_item_id, date) => {
     return axios
     .post(
-        "http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/favourite",
+        `http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/favourite`,
         {
             art_item_id: art_item_id,
             date: date,
@@ -70,7 +45,7 @@ export const like = async (token, art_item_id, date) => {
 export const comment = async (token, art_item_id, date, comment_text) => {
     return axios
     .post(
-        "http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/comment",
+        `http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/comment`,
         {
             art_item_id: art_item_id,
             text: comment_text,
