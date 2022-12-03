32d619b50ddd71fcf390a55810ba1aad13b44d5d
Yamur <yagmur.goktas@eatron.com>
2022-12-03 23:46:09 +0300

    getFollowers request

--- art-community-platform/mobile/screens/services/GeneralServices.js
+++ art-community-platform/mobile/screens/services/GeneralServices.js
@@ -17,3 +17,23 @@ export const getProfile = async (userId, token) => {
       return error.response;
     });
 };
+
+export const getFollowers = async (userId, token) => {
+  return axios
+    .get(
+      `http://ec2-44-202-130-117.compute-1.amazonaws.com/api/v1/users/${userId}/followers`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+      }
+    )
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return error.response;
+    });
+};
+
