3f5bcd8d4345601282a2107213ff66e5afa283ea
Burak Ferit Aktan <aktan.burakfa@gmail.com>
2022-12-03 19:49:05 +0300

    Added a sample model

--- art-community-platform/backend-annotations/app/core/admin.py
+++ art-community-platform/backend-annotations/app/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Sample
+
+
+admin.site.register(Sample)
--- art-community-platform/backend-annotations/app/core/models.py
+++ art-community-platform/backend-annotations/app/core/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Sample(models.Model):
+    attachment = models.FileField()
