7881a9445c0f39c53fc19c71c551bd029595656e
oguzhanozboyaci <oguzhan.ozboyaci@gmail.com>
2022-12-04 23:04:27 +0300

    Exhibition Page Edited
    Top navigation bar is added to exhibiton page

--- art-community-platform/mobile/screens/Exhibitons.js
+++ art-community-platform/mobile/screens/Exhibitons.js
@@ -1,11 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Pressable, ScrollView, StyleSheet, Text, View,
+  Image, TextInput, nav,
+  SafeAreaView,
+  Dimensions
+} from "react-native";
+import Colors from "./constants/Colors";
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Exhibitions = () => {
+function CreateExhibition() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Exhibitions Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Exhibition!</Text>
     </View>
   );
-};
+}
 
-export default Exhibitions;
+function MyExhibitions() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>My Exhibitions!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+      <Tab.Navigator>
+        <Tab.Screen name="Creacte Exhibition" component={CreateExhibition} />
+        <Tab.Screen name="My Exhibitions" component={MyExhibitions} />
+      </Tab.Navigator>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    paddingHorizontal: 12,
+    marginTop: 100,
+    marginBottom: 30,
+    backgroundColor: Colors.primary,
+    borderRadius: 6,
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  
+  buttonText: {
+    color: "white",
+  },
+});
