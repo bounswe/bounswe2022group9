7ba48e20d578cffccc82ef7ac4aa23a9aeca069f
oguzhanozboyaci <oguzhan.ozboyaci@gmail.com>
2022-12-04 23:40:58 +0300

    Profile photo update
    Profile photo update is added without functionality

--- art-community-platform/mobile/screens/Settings.js
+++ art-community-platform/mobile/screens/Settings.js
@@ -1,18 +1,31 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable, ScrollView, StyleSheet, Text, View,
+  Image, 
+  SafeAreaView,
+} from "react-native";
 import Colors from "./constants/Colors";
+import React from "react";
 
 const Settings = (props) => {
   const { navigation } = props;
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Settings Screen</Text>
-      <Pressable
-        style={styles.button}
-        onPress={() => navigation.navigate("Login")}
-      >
-        <Text>Logout</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.header}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: "https://bootdey.com/img/Content/avatar/avatar3.png" }}
+          />
+          <Pressable
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Change Profile Photo</Text>
+          </Pressable>
+        </View>
+
+        
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -21,6 +34,30 @@ export default Settings;
 const styles = StyleSheet.create({
   button: {
     padding: 8,
+    paddingHorizontal: 12,
+    marginTop: 80,
+    marginLeft: 180,
     backgroundColor: Colors.primary,
+    borderRadius: 6,
+    alignSelf: "center",
+  },
+  buttonText: {
+    color: "white",
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "stretch",
+    position: "absolute",
+    marginTop: 35,
+    marginLeft: 35,
+  },
+  header: {
+    backgroundColor: "#173679",
+    height: 200,
   },
 });
