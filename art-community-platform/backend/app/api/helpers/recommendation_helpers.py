028c3477b2980eacb322e379aae9bfab3e1fc477
Burak Ferit Aktan <aktan.burakfa@gmail.com>
2022-12-16 22:13:32 +0300

    added get_tags_od_favourites and is_new_user_for_art_recommendation

--- art-community-platform/backend/app/api/helpers/recommendation_helpers.py
+++ art-community-platform/backend/app/api/helpers/recommendation_helpers.py
@@ -11,8 +11,33 @@ def popular_art_items():
     # we are sorting the arts according to sum of their favourite count and comment count
     arts = sorted(arts, key=lambda x: (len(x.favourites)+len(x.comments)), reverse=True)
 
-    if len(arts) < number_of_arts_to_recommend_new_users:
-        return arts
+    art_ids = [a.id for a in arts]
 
-    return arts[:number_of_arts_to_recommend_new_users]
+    if len(arts) < number_of_arts_to_recommend_new_users:
+        return art_ids
+
+    return art_ids[:number_of_arts_to_recommend_new_users]
+
+
+"""
+there are different criterias for deciding whether a user is new or not,
+the function below is new user detection method for art recommendation
+"""
+def is_new_user_for_art_recommendation(user):
+    return len(user.favourites) < 5
+
+
+def get_tags_of_favourites(user):
+    tags = set()
+    for art_id in user.favourites:
+        try:
+            tags_in_this_art = ArtItem.objects.get(id=art_id).tags
+            for t in tags_in_this_art:
+                tags.add(t)
+        except:
+            pass
+    return tags
+
+
+
 
