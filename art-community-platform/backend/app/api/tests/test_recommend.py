3eda0f68519014c85021b9a65f96bcd9110c4448
Burak Ferit Aktan <aktan.burakfa@gmail.com>
2022-12-22 21:47:19 +0300

    added setUp function that will run before each unit test

---
+++ art-community-platform/backend/app/api/tests/test_recommend.py
@@ -0,0 +1,89 @@
+from django.test import TestCase, Client
+from django.test.client import RequestFactory
+from django.urls import reverse
+import json
+from ..models.art_item import ArtItem
+from faker import Faker
+import random
+
+from ..views.auth import signup
+from ..views.user import *
+from ..views.follow import *
+from ..views.profile import *
+from ..views.art_item import *
+from ..views.recommend import *
+
+fake = Faker()
+
+date = fake.date()
+
+# we will have 3 users
+usernames = [fake.username() for i in range(3)]
+passwords = [fake.password() for i in range(3)]
+emails = [fake.email() for i in range(3)]
+names = [fake.name() for i in range(3)]
+birthdates = [fake.date() for i in range(3)]
+tokens = [hashlib.sha256((usernames[i] + passwords[i] + emails[i]).encode('utf-8')).hexdigest() for i in range(3)]
+headers = [{"HTTP_AUTHORIZATION": tokens[i]} for i in range(3)]
+
+
+class TestProfile(TestCase):
+
+    def setUp(self):
+        # Setup run before every test method.
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+        # we will have 3 users
+        self.users = [User.objects.create(
+                                        name=names[i], birthdate=birthdates[i], username=usernames[i],
+                                        password=passwords[i], email=emails[i], token=tokens[i]
+                                        ) for i in range(3)]
+        self.arts = []
+
+        # we will have 30 arts,
+        # 20 of them will have tag: impressionist
+        # 10 of them will have tag: medieval
+
+        # user3 will add first 10 impressionist arts and first 10 medieval arts to her/his likes
+        # user2 will add first 5 medieval arts to her/his arts
+        for i in range(10):
+            self.arts.append(
+                ArtItem.objects.create(
+                                        tags=["impressionist"],
+                                        favourites=[self.users[2].id]
+                                      )
+                            )
+        for i in range(10):
+            self.arts.append(
+                ArtItem.objects.create(
+                                        tags=["impressionist"],
+                                      )
+                            )
+        for i in range(5):
+            self.arts.append(
+                ArtItem.objects.create(
+                                        tags=["medieval"],
+                                        favourites=[self.users[1].id, self.users[2].id]
+                                      )
+                            )
+        for i in range(5):
+            self.arts.append(
+                ArtItem.objects.create(
+                                        tags=["medieval"],
+                                        favourites=[self.users[2].id]
+                                      )
+                            )
+
+
+    def test_users_having_less_than_five_favourites(self):
+        req = self.factory.get('/api/v1/users/'+str(self.user.id)+'/get_profile_info', content_type="application/json")
+        res = get_profile_info(req, self.user.id)
+        res_json = json.loads(res.content)
+
+        self.assertEqual(res.status_code, 200)
+
+        # check the values
+        self.assertEqual(res_json['profile_img_url'], self.user.profile_img_url)
+        self.assertEqual(res_json['birthdate'], self.user.birthdate)
+        self.assertEqual(res_json['location'], self.user.location)
