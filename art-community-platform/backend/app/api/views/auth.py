6e8b5a3c23e851635e3074608a5c9b4028f06fcd
ozdemirfurkan <furkan.ozdemir1@boun.edu.tr>
2022-12-24 00:03:47 +0300

    image upload functionality fixed.

--- art-community-platform/backend/app/api/views/auth.py
+++ art-community-platform/backend/app/api/views/auth.py
@@ -2,17 +2,8 @@ from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from rest_framework.decorators import api_view
 import hashlib
 import json
-
-from ..helpers.comment_helpers import get_comment_by_id_helper
-from ..helpers.exhibition_helpers import get_exhibition_by_id_helper
-from ..helpers.notification_helpers import get_notification_by_id_helper
-from ..helpers.tag_helpers import get_tag_by_id_helper
-from ..helpers.art_item_helpers import *
 from ..helpers.user_helpers import *
 from ..models.user import User
-from ..models.art_item import ArtItem
-from ..models.comment import Comment
-from ..models.tag import Tag
 
 
 @api_view(['POST'])
