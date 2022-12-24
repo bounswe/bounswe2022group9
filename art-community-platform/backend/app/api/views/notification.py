33c679284bf192fcaf0bcadb28637d1b728de1ee
Burak Ferit Aktan <aktan.burakfa@gmail.com>
2022-12-24 16:49:22 +0300

    implemented an endpoint to returning notifications and added the url to urls.py

--- art-community-platform/backend/app/api/views/notification.py
+++ art-community-platform/backend/app/api/views/notification.py
@@ -1,19 +1,12 @@
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from rest_framework.decorators import api_view
-import hashlib
 import json
 
-from ..helpers.comment_helpers import get_comment_by_id_helper
-from ..helpers.exhibition_helpers import get_exhibition_by_id_helper
-from ..helpers.notification_helpers import get_notification_by_id_helper
-from ..helpers.tag_helpers import get_tag_by_id_helper
-from ..helpers.art_item_helpers import *
-from ..helpers.user_helpers import *
-from ..models.user import User
+
 from ..models.notification import Notification
-from ..models.art_item import ArtItem
-from ..models.comment import Comment
-from ..models.tag import Tag
+from ..models.user import User
+from ..helpers.notification_helpers import get_notification_by_receiver_id_helper
+
 
 
 @api_view(['GET'])
@@ -29,4 +22,15 @@ def get_notification_by_id(req, notification_id):
         return HttpResponse('response can not created', status=404)
 
     return JsonResponse(resp)
+
+
+@api_view(['GET'])
+def get_notification_by_receiver_id(req, receiver_id):
+    # check if receiver id is valid
+    try:
+        u = User.objects.get(id=receiver_id)
+    except:
+        return HttpResponseBadRequest("no such user with given id")
+    # return notifications coming to the user
+    return JsonResponse({"notifications": get_notification_by_receiver_id_helper()})
 
