031fb2221d0a0a33a7c042de8012a33308b02121
Yamur <yagmur.goktas@eatron.com>
2022-12-24 23:12:52 +0300

    Recommendations part

--- art-community-platform/mobile/screens/Feed.js
+++ art-community-platform/mobile/screens/Feed.js
@@ -12,6 +12,7 @@ import {
 } from "react-native";
 import Post from "./components/Post";
 import { getFeed } from "./services/GeneralServices";
+import { TabController } from "react-native-ui-lib";
 
 const Feed = (props) => {
   const { userId, token } = props.route.params;
@@ -24,28 +25,52 @@ const Feed = (props) => {
   }, []);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <FlatList
-        data={posts}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate("ArtItem", {
-                token: token,
-                art_item_id: item.id,
-                userId: userId,
-              })
-            }
-          >
-            <Post
-              username={item["owner_name"]}
-              uri={item["img_url"]}
-              date={item["date"]}
-              desc={item["description"]}
-            ></Post>
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
+      <TabController
+        items={[
+          { label: "Feed" },
+          { label: "Recommendations" },
+        ]}
+        asCarousel
+        initialIndex={0}
+        onChangeIndex={(index) => setIndex(index)}
+      >
+        <TabController.TabBar
+          height={40}
+          indicatorInsets={28}
+          indicatorStyle={{ backgroundColor: Colors.primary, height: 2 }}
+          selectedLabelColor={Colors.primary}
+          backgroundColor="#fdfdfd"
+        />
+        <TabController.PageCarousel>
+        <TabController.TabPage index={0} lazy>
+          <FlatList
+            data={posts}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.navigate("ArtItem", {
+                    token: token,
+                    art_item_id: item.id,
+                    userId: userId,
+                  })
+                }
+              >
+                <Post
+                  username={item["owner_name"]}
+                  uri={item["img_url"]}
+                  date={item["date"]}
+                  desc={item["description"]}
+                ></Post>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </TabController.TabPage>
+        <TabController.TabPage index={1} lazy>
+            <Text>Recommendations</Text>
+        </TabController.TabPage>
+        </TabController.PageCarousel>
+      </TabController>
     </View>
   );
 };
